20d4b89c1d6e4b6e4c7236c3df070162fcb9c044
Orcim <37238200+0rcim@users.noreply.github.com>
2019-03-03 17:36:25 +0800

    _global_add_cookie_function

--- public/js/_global.js
+++ public/js/_global.js
@@ -398,8 +398,44 @@ function my_utils(){
                 that.scrollTo(document.documentElement, 0, 800);
             }
         }
-    }
+    };
     this.scrollToTop();
+    // cookies
+    this.setCookie = function(key, value, t){
+        var oDate=new Date();
+        oDate.setDate(oDate.getDate()+t);
+        document.cookie=key+"="+encodeURIComponent(value)+";expires="+oDate.toUTCString();
+        if(that.getCookie(key)){
+            alert('设置成功');
+        }else{
+            alert('设置失败');
+        }
+    };
+    this.getCookie = function(key){
+        var str=document.cookie.replace(/;\s*/,';');
+        var cookieArr=str.split(';');
+        var cookieObj={};
+        var len=cookieArr.length;
+        for(var i=0; i<len; i++){
+            var item=cookieArr[i];
+            var k=item.split('=')[0];
+            var v=item.split('=')[1];
+            cookieObj[k]=v;
+        }
+        if(cookieObj[key]){
+            return decodeURIComponent(cookieObj[key]);
+        }else{
+            return false;
+        }
+    };
+    this.removeCookie = function(key){
+        document.cookie=key+"=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        if(!that.getCookie(key)){
+            alert('删除成功');
+        }else{
+            alert('删除失败');
+        }
+    }
 }
 function createRipple(obj, offset){
     var oLeft = offset?offset.x:0;
