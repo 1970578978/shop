536a08b234ec9856a6ec867d9f9b3c045e3075e9
1970578978 <1970578978@qq.com>
2019-04-08 21:03:07 +0800

    设置所有api跨域

--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('carousel_map', 'Api\CarouselMapController@show');
 
 //添加passport路由
-Route::post('login', 'Api\PassportController@login');
+Route::post('login', 'Api\PassportController@login')->middleware('cors');//允许跨域
 Route::post('register', 'Api\PassportController@register')->middleware('cors');//允许跨域
 //重置密码
-Route::post('forgot-password', 'Api\auth\ForgotPasswordController@passwordEmail');
-Route::post('resent-password', 'Api\auth\ForgotPasswordController@resetPassword');
+Route::post('forgot-password', 'Api\auth\ForgotPasswordController@passwordEmail')->middleware('cors');//允许跨域
+Route::post('resent-password', 'Api\auth\ForgotPasswordController@resetPassword')->middleware('cors');//允许跨域
 
 //必须登录的路由
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('get-details', 'Api\PassportController@getDetails');
-    Route::post('confirm', 'Api\auth\VerificationController@verificationEmail');
-    Route::post('resend-email', 'Api\auth\VerificationController@resendEmail');
+    Route::post('get-details', 'Api\PassportController@getDetails')->middleware('cors');//允许跨域
+    Route::post('confirm', 'Api\auth\VerificationController@verificationEmail')->middleware('cors');//允许跨域
+    Route::post('resend-email', 'Api\auth\VerificationController@resendEmail')->middleware('cors');//允许跨域
     
 });
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Route::get('/register', 'Home\AuthController@register')->name('register');
 Route::get('/login', 'Home\AuthController@login')->name('login');
 Route::get('/forgot-password', 'Home\AuthController@forgot_password')->name('forgotpassword');
 Route::get('/resent-password', 'Home\AuthController@reset_password')->name('resetpassword');
+
+//需要处于登录状态
 Route::get('/verifi-email', 'Home\AuthController@verifi_email')->name('verifiEmail');
 Route::get('/needEmail', 'Home\AuthController@need_verify_email');
