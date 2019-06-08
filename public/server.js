282dc2fab3b3453772d972ec5d25aebfc872d1bb
Orcim <37238200+0rcim@users.noreply.github.com>
2019-06-08 22:03:37 +0800

    确认订单界面基本完工

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-// var content = {"LEVELS":{"lv0":"吉林","lv1":"松原","lv2":"扶余县","lv3":"建设路0123号小区123栋88888888888888"},"isDefault":false,"TAG":"家","RECEIVER":"王五","TEL":"18577569963"};
+var content = {"LEVELS":{"lv0":"吉林","lv1":"松原","lv2":"扶余县","lv3":"建设路0123号小区123栋88888888888888"},"isDefault":false,"TAG":"家","RECEIVER":"王五","TEL":"18577569963"};
 // var content = fs.readFileSync(path.join(__dirname, "content.json"), "utf-8");
 // var content = {
 //     "fg_text": "文",
@@ -53,119 +53,119 @@ const path = require("path");
 //         ]
 //     }
 // ];
-var content = {
-    "product-info": {},
-    "comments": [
-        {
-            "avatars-config": {
-                "fg_text": "一",
-                "fg_color": "#1a73e8",
-                "bg_color": "#4082fc",
-                "bg_type": 2
-            },
-            "user-name": "文文问",
-            "post-date": "2019-5-18",
-            "s": 100,
-            "said-what": "很好，，，，很不错，，好，，，，，很好，，，，很不错，，，，。。。"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "二",
-                "fg_color": "#84ac52",
-                "bg_color": "#25ca48",
-                "bg_type": 0
-            },
-            "user-name": "用户2",
-            "post-date": "2019-5-10",
-            "s": 70,
-            "said-what": "just so so"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "三",
-                "fg_color": "#594863",
-                "bg_color": "#ac5385",
-                "bg_type": 1
-            },
-            "user-name": "用户3",
-            "post-date": "2019-4-28",
-            "s": 60,
-            "said-what": "OKOKOOKOKOKOKOKKOK海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjOKOKOKOKOKOKOKOK"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "四",
-                "fg_color": "#894acc",
-                "bg_color": "#cca498",
-                "bg_type": 3
-            },
-            "user-name": "用户3",
-            "post-date": "2019-4-9",
-            "s": 40,
-            "said-what": "OKOKOOKOKOKOKOKKOKOKOKOKOKO海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjKOKOKOK"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "五",
-                "fg_color": "#88aacc",
-                "bg_color": "#ccaa88",
-                "bg_type": 4
-            },
-            "user-name": "用户3",
-            "post-date": "2019-4-2",
-            "s": 20,
-            "said-what": "海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskj"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "六",
-                "fg_color": "#894acc",
-                "bg_color": "#cca498",
-                "bg_type": 3
-            },
-            "user-name": "用户3",
-            "post-date": "2019-3-21",
-            "s": 80,
-            "said-what": "OKOKOOKOKOKOKOKKOKOKOKOKOKO海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjKOKOKOK"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "七",
-                "fg_color": "#88aacc",
-                "bg_color": "#ccaa88",
-                "bg_type": 4
-            },
-            "user-name": "用户3",
-            "post-date": "2019-3-18",
-            "s": 90,
-            "said-what": "海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskj"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "八",
-                "fg_color": "#894acc",
-                "bg_color": "#cca498",
-                "bg_type": 3
-            },
-            "user-name": "用户3",
-            "post-date": "2019-3-15",
-            "s": 100,
-            "said-what": "OKOKOOKOKOKOKOKKOKOKOKOKOKO海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjKOKOKOK"
-        },
-        {
-            "avatars-config": {
-                "fg_text": "九",
-                "fg_color": "#88aacc",
-                "bg_color": "#ccaa88",
-                "bg_type": 4
-            },
-            "user-name": "用户3",
-            "post-date": "2019-3-10",
-            "s": 10,
-            "said-what": "海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskj"
-        }
-    ]
-}
+// var content = {
+//     "product-info": {},
+//     "comments": [
+//         {
+//             "avatars-config": {
+//                 "fg_text": "一",
+//                 "fg_color": "#1a73e8",
+//                 "bg_color": "#4082fc",
+//                 "bg_type": 2
+//             },
+//             "user-name": "文文问",
+//             "post-date": "2019-5-18",
+//             "s": 100,
+//             "said-what": "很好，，，，很不错，，好，，，，，很好，，，，很不错，，，，。。。"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "二",
+//                 "fg_color": "#84ac52",
+//                 "bg_color": "#25ca48",
+//                 "bg_type": 0
+//             },
+//             "user-name": "用户2",
+//             "post-date": "2019-5-10",
+//             "s": 70,
+//             "said-what": "just so so"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "三",
+//                 "fg_color": "#594863",
+//                 "bg_color": "#ac5385",
+//                 "bg_type": 1
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-4-28",
+//             "s": 60,
+//             "said-what": "OKOKOOKOKOKOKOKKOK海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjOKOKOKOKOKOKOKOK"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "四",
+//                 "fg_color": "#894acc",
+//                 "bg_color": "#cca498",
+//                 "bg_type": 3
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-4-9",
+//             "s": 40,
+//             "said-what": "OKOKOOKOKOKOKOKKOKOKOKOKOKO海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjKOKOKOK"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "五",
+//                 "fg_color": "#88aacc",
+//                 "bg_color": "#ccaa88",
+//                 "bg_type": 4
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-4-2",
+//             "s": 20,
+//             "said-what": "海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskj"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "六",
+//                 "fg_color": "#894acc",
+//                 "bg_color": "#cca498",
+//                 "bg_type": 3
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-3-21",
+//             "s": 80,
+//             "said-what": "OKOKOOKOKOKOKOKKOKOKOKOKOKO海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjKOKOKOK"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "七",
+//                 "fg_color": "#88aacc",
+//                 "bg_color": "#ccaa88",
+//                 "bg_type": 4
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-3-18",
+//             "s": 90,
+//             "said-what": "海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskj"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "八",
+//                 "fg_color": "#894acc",
+//                 "bg_color": "#cca498",
+//                 "bg_type": 3
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-3-15",
+//             "s": 100,
+//             "said-what": "OKOKOOKOKOKOKOKKOKOKOKOKOKO海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskjKOKOKOK"
+//         },
+//         {
+//             "avatars-config": {
+//                 "fg_text": "九",
+//                 "fg_color": "#88aacc",
+//                 "bg_color": "#ccaa88",
+//                 "bg_type": 4
+//             },
+//             "user-name": "用户3",
+//             "post-date": "2019-3-10",
+//             "s": 10,
+//             "said-what": "海星星星星星星星星星星星星星星星星星星星星星星星星，还行世纪东方松岛枫哦你师弟还是对赌关乎深度覆盖u很多覅古话说丢货\nkabskj"
+//         }
+//     ]
+// }
 http.createServer(function(req, res){
     res.writeHead(200, {"content-type": "text/html;charset=utf-8", "Access-Control-Allow-Origin": "*"});
     if(req.url != "/favicon.ico"){
